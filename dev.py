87295e6ba87c05564e92994dd83b93487d4e787b
fiandev <wongkeras2@gmail.com>
2023-11-13 05:20:06 +0700

    update: structure folder

--- dev.py
+++ dev.py
@@ -59,4 +59,4 @@ def lahelu_downloader ():
     
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
--- main.py
+++
@@ -1,4 +0,0 @@
-from application.core.server import server
-
-if __name__ == "__main__":
-    server.run(debug=True)
