317bece807638471da2a861922b8c1cd7bcf764d
fiandev <wongkeras2@gmail.com>
2023-11-20 12:06:01 +0700

    feat: split vite and flask view

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    outDir: './static/'
-  }
   plugins: [
     react(),
   ],
