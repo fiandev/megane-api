ccb8c2b5e60625656aca4a1601ab430a4c1d0fb1
fiandev <wongkeras2@gmail.com>
2023-11-22 22:46:48 +0700

    [build]: update react build

--- src/components/molecules/FeatureList.jsx
+++ src/components/molecules/FeatureList.jsx
@@ -1,6 +1,6 @@
 import { SiFastapi } from "react-icons/si";
 
-export default function FeatureList() {
+export default function FeatureList({ feature }) {
   return (
     <div className="flex items-start justify-center gap-2 p-2 bg-secondary rounded-md shadow-md">
       <div className="flex items-center justify-center w-1/4 h-full">
@@ -10,7 +10,7 @@ export default function FeatureList() {
       </div>
       <div className="flex flex-col items-start justify-end text-start">
         <h1 className="text-light text-lg capitalize font-semibold">
-          featured
+          
         </h1>
         <p className="text-light text-sm">
           Selamat datang di papan klip Gboard, teks apa pun yang Anda salin akan
--- src/components/organisms/LandingFeature.jsx
+++ src/components/organisms/LandingFeature.jsx
@@ -1,4 +1,7 @@
-import FeatureList from "../molecules/FeatureList";
+// import FeatureList from "../molecules/FeatureList";
+import { MdMoneyOff } from "react-icons/md";
+import { FaShield, FaRobot } from "react-icons/fa6";
+import { FaBook, FaRocket } from "react-icons/fa";
 
 export default function LandingFeature() {
   return (
@@ -12,12 +15,72 @@ export default function LandingFeature() {
         </h2>
       </div>
 
-      <div className="w-full grid md:grid-cols-2 lg:grid-cols-2 gap-2 lg:gap-4 place-content-center">
-        {Array(4)
-          .fill(1)
-          .map((v) => {
-            return <FeatureList />;
-          })}
+      <div className="w-full grid md:grid-cols-2 lg:grid-cols-2 gap-2 lg:gap-4">
+        
+        <div className="flex items-center justify-center gap-2 h-40 p-2 overflow-auto bg-secondary rounded-md shadow-md">
+          <div className="flex items-center justify-center w-1/4 h-full">
+            <div className="flex items-center justify-center w-full h-full text-xl rounded-lg text-light">
+              <FaRocket />
+            </div>
+          </div>
+          <div className="w-3/4 flex flex-col items-start justify-end text-start">
+            <h1 className="text-light text-lg capitalize font-semibold">
+              fast and efficient performance
+            </h1>
+            <p className="text-light text-sm">
+              The available API is built using the Python programming language so there is no need to doubt its speed and efficiency.
+            </p>
+          </div>
+        </div>
+        
+        <div className="flex items-center justify-center gap-2 h-40 p-2 overflow-auto bg-secondary rounded-md shadow-md">
+          <div className="flex items-center justify-center w-1/4 h-full">
+            <div className="flex items-center justify-center w-full h-full text-xl rounded-lg text-light">
+              <MdMoneyOff />
+            </div>
+          </div>
+          <div className="w-3/4 flex flex-col items-start justify-end text-start">
+            <h1 className="text-light text-lg capitalize font-semibold">
+              free
+            </h1>
+            <p className="text-light text-sm">
+              you don't need to spend money to use the available APIs
+            </p>
+          </div>
+        </div>
+        
+        <div className="flex items-center justify-center gap-2 h-40 p-2 overflow-auto bg-secondary rounded-md shadow-md">
+          <div className="flex items-center justify-center w-1/4 h-full">
+            <div className="flex items-center justify-center w-full h-full text-xl rounded-lg text-light">
+              <FaBook />
+            </div>
+          </div>
+          <div className="w-3/4 flex flex-col items-start justify-end text-start">
+            <h1 className="text-light text-lg capitalize font-semibold">
+              complete documentation
+            </h1>
+            <p className="text-light text-sm">
+              We try to make API documentation easy to read and understand
+            </p>
+          </div>
+        </div>
+        
+        <div className="flex items-center justify-center gap-2 h-40 p-2 overflow-auto bg-secondary rounded-md shadow-md">
+          <div className="flex items-center justify-center w-1/4 h-full">
+            <div className="flex items-center justify-center w-full h-full text-xl rounded-lg text-light">
+              <FaRobot />
+            </div>
+          </div>
+          <div className="w-3/4 flex flex-col items-start justify-end text-start">
+            <h1 className="text-light text-lg capitalize font-semibold">documentation
+              supports integration with bots
+            </h1>
+            <p className="text-light text-sm">
+              Our API really supports integration with bots because it uses the Json file format
+            </p>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
